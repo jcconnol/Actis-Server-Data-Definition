700151f2b1d37e0ef8ea34e0b9a3bf121a46f9eb
jcconnol <jcconnol4@gmail.com>
2019-09-19 10:49:50 -0500

    product to command
    edit product to fit with command database.

--- Command.sql
+++ Command.sql
@@ -1,14 +1,14 @@
 
 CREATE TABLE command (
-  name character varying(32) NOT NULL DEFAULT(''), --Stored in the edu.uark.models.entities:ProductEntity#lookupCode property. See also the named constant defined in edu.uark.models.entities.fieldnames:ProductFieldNames that is used for Java <-> SQL mappings.
-  status boolean NOT NULL DEFAULT(false), --Stored in the edu.uark.models.entities:ProductEntity#count property. See also the named constant defined in edu.uark.models.entities.fieldnames:ProductFieldNames that is used for Java <-> SQL mappings.
+  name character varying(32) NOT NULL DEFAULT(''),
+  status boolean NOT NULL DEFAULT(false),
   extra character varying(255),
   CONSTRAINT command_pkey PRIMARY KEY (name)
 );
 
 --DROP INDEX ix_command_name;
 
-CREATE INDEX ix_product_lookupcode --An index on the command table name column
+CREATE INDEX ix_command_lookupcode --An index on the command table name column
   ON command
   USING btree
   (lower(name::text) COLLATE pg_catalog."default");
